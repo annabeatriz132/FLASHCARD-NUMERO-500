f94a949d060476c3e92aed44ffaaf55865ff6db9
anna a melhor <162126959+annabeatriz132@users.noreply.github.com>
2025-07-29 12:28:09 -0300

    Add files via upload

---
+++ FLASHCARD-NUMERO-500-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+'Katseye',
+'O que é o Katseye?',
+'É um grupo feminino descrito como "grupo feminino global"'
+)
+
+criaCartao(  
+    'Katseye integrantes',
+    'Qual o nome das integrantes?',
+    'Lara, Daniela, Megan, Sophia, Manon e Yoonchae'
+)
+
+criaCartao(
+    'Katseye idade',
+    'Qual a idade delas?',
+    'manon 23, Yoonchae 17, Sophia 22, Daniela 21, Lara 19 e Megan 19'
+)
